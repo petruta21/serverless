156e264cfc71ec22d28fab1f76ede9792bc4923e
Tatsiana Petushenka <tatsiana_petrushenka@epam.com>
2024-09-10 22:02:15 +0200

    task08 implementation

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -2,31 +2,44 @@ package com.task08;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
-import com.syndicate.deployment.model.lambda.url.AuthType;
-import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import com.task08.sdk.OpenMeteoAPI;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-    lambdaName = "api_handler",
-	roleName = "api_handler-role",
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        lambdaName = "api_handler",
+        roleName = "api_handler-role",
+        layers = {"sdk-layer"},
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@LambdaUrlConfig(
-		authType = AuthType.NONE,
-		invokeMode = InvokeMode.BUFFERED
+@LambdaUrlConfig
+@LambdaLayer(
+        layerName = "sdk-layer",
+        libraries = {"lib/task08-1.0.0-sdk.jar"},
+        runtime = DeploymentRuntime.JAVA11
 )
-public class ApiHandler implements RequestHandler<Object, Map<String, Object>> {
+public class ApiHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("body", "Hello from Lambda");
-		return resultMap;
-	}
+    private final Map<String, String> responseHeaders = Map.of("Content-Type", "application/json");
+
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
+        String forecastString = OpenMeteoAPI.getWeatherForecast();
+        String path = requestEvent.getRequestContext().getHttp().getPath();
+        APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+        if ("/weather".equalsIgnoreCase(path)) {
+            response.setStatusCode(200);
+            response.setBody(forecastString);
+        } else {
+            response.setStatusCode(400);
+        }
+        response.setHeaders(responseHeaders);
+        return response;
+    }
 }
