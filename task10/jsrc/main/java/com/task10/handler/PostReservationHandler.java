83acd76a822bf0538e2d120503d0c0c6f185cb2b
Tatsiana Petushenka <tatsiana_petrushenka@epam.com>
2024-09-15 13:28:18 +0200

    task10 more validation checks, check is table not found

--- task10/jsrc/main/java/com/task10/handler/PostReservationHandler.java
+++ task10/jsrc/main/java/com/task10/handler/PostReservationHandler.java
@@ -1,16 +1,14 @@
 package com.task10.handler;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
-import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.*;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.task10.dto.*;
+import com.task10.handler.util.DynamoDBHelper;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
 
 import java.util.UUID;
@@ -26,6 +24,7 @@ public class PostReservationHandler extends CognitoSupport implements RequestHan
 
     private final String tableName = System.getenv("RESERVATIONS_TABLE_NAME");
     private final DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
+    private final String tableNameTables = System.getenv("TABLES_TABLE_NAME");
 
 
     @Override
@@ -34,7 +33,30 @@ public class PostReservationHandler extends CognitoSupport implements RequestHan
         String requestBody = requestEvent.getBody();
         try {
             AddReservationRequest addReservationRequest = objectMapper.readValue(requestBody, AddReservationRequest.class);
+
+            String validationError = validateRequest(addReservationRequest);
+
+            if (validationError != null) {
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(400)
+                        .withBody("{\"message\": \"Bad Request: " + validationError + "\"}");
+            }
+
             ReservationDBEntity reservationDBEntity = convertToReservationDBEntity(addReservationRequest);
+
+          /*  if (isConflictingReservation(reservationDBEntity)) {
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(400)
+                        .withBody("{\"message\": \"This table is already reserved.\"}");
+            }*/
+
+          if(!isTableExist(reservationDBEntity.getTableNumber())){
+                return new APIGatewayProxyResponseEvent()
+                        .withStatusCode(400)
+                        .withBody("{\"message\": \"This table does not exist.\"}");
+            }
+
+
             putToDynamoDB(reservationDBEntity);
 
             AddReservationResponse response = new AddReservationResponse();
@@ -81,6 +103,23 @@ public class PostReservationHandler extends CognitoSupport implements RequestHan
         PutItemOutcome outcome = table.putItem(item);
 
         System.out.println("Reservation saved with outcome: " + outcome);
+    }
+
+    private String validateRequest(AddReservationRequest request) {
+        return request.getTableNumber() <= 0 ? "Table number must be greater than 0."
+                : request.getClientName().isEmpty() ? "Client name is a mandatory field."
+                : request.getPhoneNumber().isEmpty() ? "Phone number is a mandatory field."
+                : request.getDate().isEmpty() ? "Date is a mandatory field."
+                : request.getSlotTimeStart().isEmpty() ? "Slot time start is a mandatory field."
+                : request.getSlotTimeEnd().isEmpty() ? "Slot time end is a mandatory field."
+                : null;
+    }
+
+    private boolean isTableExist(int tableNumber)
+    {
+        return DynamoDBHelper.getFromDynamoDB(dynamoDB, tableNameTables)
+                .stream()
+                .anyMatch(existingTable -> existingTable.getNumber() == tableNumber);
     }
 
 }
